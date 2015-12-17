9770f43ac6de637e7918418cec146887ec80f0dc
ace411 <lochbm@live.com>
2015-12-17 19:46:56 +0300

    Create upload.php
    This file holds the file upload form which will allow users to upload multiple images to the server.

---
+++ upload.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Upload an image</title>
+	<link rel="stylesheet" type="text/css" href="css/lightbox.css">
+</head>
+<body>
+<h2 align="center">File upload script</h2>
+<form action="confirm_upload.php" method="post" id="upload_form" enctype="multipart/form-data">
+    <fieldset>
+		<input type="file" name="files[]" class="upload_img" multiple>
+		<input type="submit" value="Upload image">
+    </fieldset>
+</form>
+</body>
+</html>
